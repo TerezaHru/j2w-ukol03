1665cd33e0382e1c16e21049125d52aa87c12952
Tereza Hrušková <Ter.Hruskova@seznam.cz>
2022-10-15 22:53:41 +0200

    mapovani nenulovych hodnot

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,20 +1,37 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
  */
 @Controller
 public class VizitkaController {
+
+  private final List<Vizitka> seznamZamestnancu = List.of(
+          new Vizitka("Dita (Přikrylová) Formánková", "Czechitas z. s.", "Václavské náměstí 837/11", "11000","", "dita@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
+          new Vizitka("Barbora Bühnová", "", "Václavské náměstí 837/11", "d","11000", "", "+420 800123456", ""),
+          new Vizitka("Monika Ptáčníková", "Czechitas z. s.", "Václavské náměstí 837/11", "11000"," Praha 1", "monika@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
+          new Vizitka("Mirka Zatloukalová", "Czechitas z. s.", "Václavské náměstí 837/11", "11000"," Praha 1", "mirka@czechitas.cs", "+420 800123456", "www.czechitas.cz")
+  );
+
+
   @GetMapping("/")
-  public String seznam() {
-    return "seznam";
+
+    public ModelAndView seznam() {
+      ModelAndView modelAndView = new ModelAndView("seznam");
+      modelAndView.addObject("zamestnanci", seznamZamestnancu);
+      return modelAndView;
   }
 
   @GetMapping("/detail")
-  public String detail() {
-    return "detail";
+  public ModelAndView detail() {
+    ModelAndView modelAndView= new ModelAndView("detail");
+    return modelAndView;
   }
 }
