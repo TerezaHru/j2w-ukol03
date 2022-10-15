cf4cfdf27f789965827350c7354402232a30388d
Tereza Hrušková <Ter.Hruskova@seznam.cz>
2022-10-15 23:11:39 +0200

    mapovani i nulovych hodnot

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -15,8 +15,8 @@ public class VizitkaController {
 
   private final List<Vizitka> seznamZamestnancu = List.of(
           new Vizitka("Dita (Přikrylová) Formánková", "Czechitas z. s.", "Václavské náměstí 837/11", "11000","", "dita@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
-          new Vizitka("Barbora Bühnová", "", "Václavské náměstí 837/11", "d","11000", "", "+420 800123456", ""),
-          new Vizitka("Monika Ptáčníková", "Czechitas z. s.", "Václavské náměstí 837/11", "11000"," Praha 1", "monika@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
+          new Vizitka("Barbora Bühnová", null, "Václavské náměstí 837/11", "d","11000", null, "+420 800123456",null),
+          new Vizitka("Monika Ptáčníková", "Czechitas z. s.", null, "11000"," Praha 1", "monika@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
           new Vizitka("Mirka Zatloukalová", "Czechitas z. s.", "Václavské náměstí 837/11", "11000"," Praha 1", "mirka@czechitas.cs", "+420 800123456", "www.czechitas.cz")
   );
 
