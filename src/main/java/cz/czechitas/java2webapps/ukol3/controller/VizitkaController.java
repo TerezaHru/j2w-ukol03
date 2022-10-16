af70c714dd6895d42d39a5c67d3c77cda86ad83f
Tereza Hrušková <Ter.Hruskova@seznam.cz>
2022-10-16 10:39:26 +0200

    mapování detailu a mapy

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -3,6 +3,8 @@ package cz.czechitas.java2webapps.ukol3.controller;
 import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -11,13 +13,14 @@ import java.util.List;
  * Kontroler obsluhující zobrazování vizitek.
  */
 @Controller
+@RequestMapping("/")
 public class VizitkaController {
 
   private final List<Vizitka> seznamZamestnancu = List.of(
-          new Vizitka("Dita (Přikrylová) Formánková", "Czechitas z. s.", "Václavské náměstí 837/11", "11000","", "dita@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
-          new Vizitka("Barbora Bühnová", null, "Václavské náměstí 837/11", "d","11000", null, "+420 800123456",null),
-          new Vizitka("Monika Ptáčníková", "Czechitas z. s.", null, "11000"," Praha 1", "monika@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
-          new Vizitka("Mirka Zatloukalová", "Czechitas z. s.", "Václavské náměstí 837/11", "11000"," Praha 1", "mirka@czechitas.cs", "+420 800123456", "www.czechitas.cz")
+          new Vizitka("Dita (Přikrylová) Formánková","Czechitas z. s.", "Václavské náměstí 11, Praha 2", "11000","", "dita@czechitas.cs", "+420 800123456", "www.czechitas.cz", "1"),
+          new Vizitka("Barbora Bühnová", "Czechitas z. s.", "Zelená lhota 4, Nýrsko", "38402"," Praha 1", "barbora@czechitas.cs", "+420 800123456","www.czechitas.cz", "2"),
+          new Vizitka("Monika Ptáčníková", "Czechitas z. s.", "Pitterova 1, Praha 3", "11000"," Praha 1", "monika@czechitas.cs", "+420 800123456", "www.czechitas.cz", "3"),
+          new Vizitka("Mirka Zatloukalová", "Czechitas z. s.", "Krásova 4, Praha 3", "11000"," Praha 1", "mirka@czechitas.cs", "+420 800123456", "www.czechitas.cz", "4")
   );
 
 
@@ -29,9 +32,10 @@ public class VizitkaController {
       return modelAndView;
   }
 
-  @GetMapping("/detail")
-  public ModelAndView detail() {
+  @GetMapping("/detail/{id}")
+  public ModelAndView detail(@PathVariable int id) {
     ModelAndView modelAndView= new ModelAndView("detail");
+    modelAndView.addObject("zamestnanec", seznamZamestnancu.get(id - 1));
     return modelAndView;
   }
 }
